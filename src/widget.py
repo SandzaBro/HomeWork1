eac9b8303ba1720aa73dd4bba18d80f0974a4d19
Евгений Королевский <evgeniy_defal@mail.ru>
2026-07-07 07:57:14 +0300

    updates

--- src/widget.py
+++ src/widget.py
@@ -8,10 +8,6 @@ def mask_account_card(info_account_card: str) -> str:
     # Разделяем строку на части
     parts = info_account_card.split()
 
-    # Проверяем, что строка не пустая
-    if not parts:
-        return info_account_card
-
     # Определяем тип (последнее слово - это номер)
     # Если в строке больше 2 слов (например, "Visa Platinum"), объединяем все кроме последнего
     if len(parts) > 1:
@@ -32,4 +28,11 @@ def mask_account_card(info_account_card: str) -> str:
         return f"{card_type} {masked_number}"
 
 
-print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
